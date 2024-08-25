cd5108ef18350d20b46b87b97f974e6924c414ca
achaconrosales <achacon@estudiantec.cr>
2024-08-24 18:33:49 -0600

    suebiendo cambios al subcircuito1

--- Tarea_1/Subcircuito1_gray_to_binary/src/design/module.sv
+++ Tarea_1/Subcircuito1_gray_to_binary/src/design/module.sv
@@ -1,16 +1,25 @@
 module gray_to_binary_leds (
-    input logic [3:0] gray,   // Entrada en código Gray de 4 bits
-    output logic [3:0] binary // Salida en binario de 4 bits
-   
+    input  logic [3 : 0] gray,  // Entrada: Código Gray (4 bits)
+    output logic [3 : 0] binary, // Salida: Código Binario (N bits)
+    output logic [3 : 0] leds // salida: leds
 );
 
-    // Convertir Gray a binario
-    assign binary[3] = gray[3];
-    assign binary[2] = gray[3] ^ gray[2];
-    assign binary[1] = binary[2] ^ gray[1];
-    assign binary[0] = binary[1] ^ gray[0];
 
-    // Controlar los LEDs en función del resultado binario
-    
+
+    // Lógica combinacional para la decodificación
+    always_comb begin
+        // gray_code a binary_code usando compuertas lógicas
+
+        binary[3] = gray [3];
+        binary[2] = gray [3] ^ gray [2];
+        binary[1] = binary [2] ^ gray [1];
+        binary[0] = binary [1] ^ gray [0];
+
+        leds[3] = ~binary[3];
+        leds[2] = ~binary[2];
+        leds[1] = ~binary[1];
+        leds[0] = ~binary[0];
+        
+    end
 
 endmodule
